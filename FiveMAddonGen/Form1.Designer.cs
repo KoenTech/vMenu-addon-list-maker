f679b1f5d2f4ce5e6f168d42b40a1143f44ca406
KoenCaspers <knf.caspers@gmail.com>
2020-08-16 15:35:24 +0200

    initial file upload

---
+++ FiveMAddonGen/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FiveMAddonGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.button1.Size = new System.Drawing.Size(339, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(339, 410);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Waiting for file...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(339, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "vMenu addon list maker - KoenTech";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FiveMAddonGen/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FiveMAddonGen
+{
+    public partial class Form1 : Form
+    {
+        int count;
+        StringBuilder stringBuilder = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open vehicles.meta File";
+            theDialog.Filter = "META files|*.meta";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                GetFromXml(theDialog.FileName.ToString());
+            }
+        }
+
+        void GetFromXml(string file)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                if (node.Name == "InitDatas")
+                {
+                    foreach (XmlNode node1 in node.ChildNodes)
+                    {
+
+                        if (node1.Name == "Item")
+                        {
+                            stringBuilder.AppendLine("    \"" + node1.ChildNodes[0].InnerText + "\",");
+                            count++;
+                        }
+                    }
+                }
+            }
+            if (stringBuilder.Length < 2) stringBuilder.Append("This File is not a vehicles.meta file!");
+            richTextBox1.Text = stringBuilder.ToString();
+            label1.Text = String.Format("Detected {0} vehicles!", count);
+            stringBuilder.Clear();
+            count = 0;
+        }
+    }
+}
